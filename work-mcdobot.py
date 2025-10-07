d917c600f4e17d5e7011c34a6528823cce5a695d
carl <carldudeletsgo@gmail.com>
2025-10-07 14:14:46 -0700

    added mcdobot

---
+++ work-mcdobot.py
@@ -0,0 +1,10 @@
+# Work - McDoBot
+# Author: Carl Dicioco
+# 6 October
+
+# It asks you if you want fries
+# It shoould accept Yes/yes or No/no
+# as inputs, and reply appropriately depending on the answer.
+# If the user inuts anything else,
+# it should repeat back ther answer and
+# say that it does not understand.
